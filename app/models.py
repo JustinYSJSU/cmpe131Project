03fa8f81a58a67d7ca47beb0985318bda891ad6d
JustinYSJSU <justin.yamamoto@sjsu.edu>
2022-04-26 18:54:19 -0700

    Added user rating total / total ratings to user database added a test user to app.db

--- app/models.py
+++ app/models.py
@@ -11,11 +11,10 @@ class User(db.Model):
  payment_method_number = db.Column(db.String(19))
  payment_method_cvc = db.Column(db.String(3))
  payment_method_expdate = db.Column(db.String(5))
- #add total number from reviews
- #add total number of reviews
- review_total_score = (db.Column(db.Integer))
- review_total = (db.Column(db.Integer))
- #cart = ? 
+ #sum of all review scores
+ review_total_score = db.Column(db.Integer)
+ #number of total reviews
+ review_total = db.Column(db.Integer)
 
  def __repr__(self):
   return f'<User {self.username} {self.email} {self.address}>'
