2965518f050049cb39cdd977ca11d9424c4dcc7c
Joe Moules <itsjoemoules@gmail.com>
2022-04-28 22:21:41 -0700

    added ability to create user account

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from app import appObj
 from app.user_login import LoginUser
+from app.createAccount import CreateUser
 
 from flask import render_template, flash, redirect, url_for
+from werkzeug.security import generate_password_hash
 
 from app import db
 from app.models import User 
@@ -31,3 +33,26 @@ def login():
 @login_required
 def home():
  return render_template('home.html')
+
+@appObj.route('/createAccount', methods = ['GET', 'POST'])
+def createAccount():
+  accountForm = CreateUser()
+  if accountForm.validate_on_submit():
+    user=User()
+    user.username=accountForm.username.data
+    user.email=accountForm.email.data
+    user.set_password(accountForm.password.data)
+    user.address=accountForm.address.data
+    user.payment_method_company=accountForm.paymentMethodCompany.data
+    user.payment_method_number=accountForm.paymentNumber.data
+    user.payment_method_expdate=accountForm.paymentExpDate.data
+    user.payment_method_cvc=accountForm.paymentCVC.data
+    db.session.add(user)
+    db.session.commit()
+    print("user has been created")
+    return redirect('/')
+  return render_template('createAccount.html', accountForm = accountForm)
+
+@appObj.route('/testing')
+def redirectTest():
+  return redirect('/')
