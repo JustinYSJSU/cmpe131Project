787f71107e94613b195da937851c165244a9baf7
Trung Huynh <trung.huynh@sjsu.edu>
2022-05-05 21:43:42 -0700

    fixed formatting of money to show $ and 2 decimal places For future reference, add "$%.2f"|format(variable_name) to any HTML requiring this format

--- app/item_sale.py
+++ app/item_sale.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired
 
 class SellItem(FlaskForm):
  item_sell_name = StringField('Item Name', validators = [DataRequired()])
- item_sell_price = DecimalField('Item Price', validators =[DataRequired()])
+ item_sell_price = DecimalField('Item Price', validators =[DataRequired()], places = 2)
  #image submit
  item_sell_desc = StringField('Description', validators = [DataRequired()])
  item_image = FileField()
