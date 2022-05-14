c8b231d511b83b344ef758467d8b46bfb8f77fc0
JustinYSJSU <justin.yamamoto@sjsu.edu>
2022-05-14 00:05:55 -0700

    Fixed forms for user creation and item sell Added character limits, integer only fields, min int value for selling

--- app/createAccount.py
+++ app/createAccount.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, IntegerField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length
 
 class CreateUser(FlaskForm):
-    username = StringField( 'Username', validators = [DataRequired()])
-    email = StringField ( 'Email', validators = [DataRequired()])
-    password = StringField( 'Password', validators = [DataRequired()])
-    address = StringField( 'Address', validators = [DataRequired()])
-    paymentMethodCompany = StringField( 'PaymentProvidor', validators = [DataRequired()])
-    paymentNumber = StringField('PaymentNumber', validators = [DataRequired()])
-    paymentCVC = StringField('PaymentCVC', validators = [DataRequired()])
-    paymentExpDate = StringField('PaymentExpDate', validators = [DataRequired()])
+    username = StringField('Username', validators = [DataRequired(),Length(min=3, max=32)], render_kw={"placeholder": "Ex: yabe, between 3 and 32 characters"})
+    email = StringField ( 'Email', validators = [DataRequired()], render_kw={"placeholder": "Ex: yabe@gmail.com"})
+    password = PasswordField( 'Password', validators = [DataRequired(), Length(min=4)], render_kw={"placeholder": "Minumum 4 characters"})
+    address = StringField( 'Address', validators = [DataRequired()], render_kw={"placeholder": "Ex: 1234 yabe Drive"})
+    paymentMethodCompany = StringField( 'PaymentProvidor', validators = [DataRequired(), Length(max=16)],render_kw={"placeholder": "Ex: Visa, max 16 characters"})
+    paymentNumber = IntegerField('PaymentNumber', validators = [DataRequired()], render_kw={"placeholder": "Ex: 1234xxxxxxxxxxxx"})
+    paymentCVC = IntegerField('PaymentCVC', validators = [DataRequired()], render_kw={"placeholder": "Ex: 123"})
+    paymentExpDate = IntegerField('PaymentExpDate', validators = [DataRequired()], render_kw={"placeholder": "Ex: 0522"})
     submit = SubmitField('Create Account')
